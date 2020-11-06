41946111e4c716bcb232ac1fbd17f1739b7d7ee3
Juan Raudales <juanraudales@unitec.edu>
2020-11-05 19:14:00 -0600

    fix linters

--- bin/main.rb
+++ bin/main.rb
@@ -1,43 +1,31 @@
 #!/usr/bin/env ruby
 puts 'Hello, world!'
 loop do
-  puts '-------------------- Welcome --------------------'
-  puts "------------------ Tic-Tac-Toe ------------------\n\n"
+  puts "-------------------- Welcome --------------------\n------------------ Tic-Tac-Toe ------------------\n\n"
   print 'X Player\'s name: '
   name1 = gets.chomp
-  print 'O Player\'s name: '
-  name2 = gets.chomp
   # Create Player class
   grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
   game_over = false
   loop do
     # Print board function here
-    puts " #{grid[0][0]} | #{grid[0][1]} | #{grid[0][2]}"
-    puts "---+---+---"
-    puts " #{grid[1][0]} | #{grid[1][1]} | #{grid[1][2]}"
-    puts "---+---+---"
-    puts " #{grid[2][0]} | #{grid[2][1]} | #{grid[2][2]}"
-    play = ''
+    puts " #{grid[0][0]} | #{grid[0][1]} | #{grid[0][2]}\n---+---+---\n#{grid[1][0]} | #{grid[1][1]} | #{grid[1][2]}"
+    puts "---+---+---\n#{grid[2][0]} | #{grid[2][1]} | #{grid[2][2]}"
     # Get play from player function here
-    puts "#{name1}\'s turn. Enter <row_number,col_number>"
-    print 'Play: '
+    print "#{name1}\'s turn. Enter <row_number,col_number>\nPlay: "
     play = gets.chomp
     row = play[0].to_i
     col = play[2].to_i
     row -= 1
     col -= 1
     if row < -1 || row > 3 || col < -1 || col > 3
-      puts '======================================='
-      puts '----------- Invalid entry -------------'
-      puts '======================================='
+      puts "================================\n----------- Invalid entry -------------\n================================"
       next
     end
     # Validate move function here
-    for i in 0..2 do
-      for j in 0..2 do
-        if i == row && j == col && "#{grid[i][j]}" == ' '
-          grid[i][j] = 'X'
-        end
+    (0..2).each do |i|
+      (0..2).each do |j|
+        grid[i][j] = 'X' if i == row && j == col && (grid[i][j]).to_s == ' '
       end
     end
     # Validate win lose or draw function here
